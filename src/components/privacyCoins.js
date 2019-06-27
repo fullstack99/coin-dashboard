1ae8d0f2dc3751ef8ec543b091049f9413aa5c84
fullstack99 <success1989127@gmail.com>
2019-06-26 22:44:52 -0600

    Remove unused code

--- src/components/privacyCoins.js
+++ src/components/privacyCoins.js
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from "react"
-// import Container from "react-bootstrap/Container"
-// import Row from "react-bootstrap/Row"
-// import Col from "react-bootstrap/Col"
 import Table from "react-bootstrap/Table"
 
 import styled from "@emotion/styled"
