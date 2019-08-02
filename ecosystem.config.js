9d9cb67182c5ec8ba8e995e33e2063cf4a0b6839
fullstack99 <success1989127@gmail.com>
2019-08-02 01:00:11 -0600

    Testing deployments

---
+++ ecosystem.config.js
@@ -0,0 +1,36 @@
+module.exports = {
+  apps : [{
+    name: 'Dashboard',
+    // script: 'npm run serve',
+    script: 'npm run develop',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    // args: 'one two',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user: 'bitnami',
+      host: '35.164.176.102',
+      key: '~/gocodistry/Coingenious.pem',
+      ref: 'origin/master',
+      repo: 'git@bitbucket.org:gocodistry/dashboard.git',
+      path: '/opt/bitnami/apps/pm2-deployments/coingenius.ai',
+      // 'post-deploy':
+      //   'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --name AuthWebsite',
+      env: {
+        NODE_ENV: 'production'
+      }
+    }
+  }
+};
