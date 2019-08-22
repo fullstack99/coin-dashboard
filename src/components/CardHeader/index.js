ee88800c25b016f1b8912c2d9f76916c68b879e6
fullstack99 <success1989127@gmail.com>
2019-08-22 14:30:05 +0530

    Apply padding whole box.

--- src/components/CardHeader/index.js
+++ src/components/CardHeader/index.js
@@ -4,8 +4,7 @@ const CardHeader = styled.section`
   width: 100%;
   height: 72px;
   border-radius: 8px;
-  padding-top: 1em;
-  padding-left: 1em;
+  padding: 1em;
   font-family: sans-serif;
   background-color: #222636;
 `
