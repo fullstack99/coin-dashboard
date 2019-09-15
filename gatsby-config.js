614d0ec167ca4f98834fc78c3f07c5bc1cd99a52
fullstack99 <success1989127@gmail.com>
2019-09-14 22:15:50 -0600

    Add missing dependencies

--- gatsby-config.js
+++ gatsby-config.js
@@ -118,6 +118,12 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/svg/logo.svg` // This path is relative to the root of the site.
       }
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        // Add any options here
+      },
     }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
