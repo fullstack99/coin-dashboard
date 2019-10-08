85e77bb53681b5f808467d7927c6364ce2eb0d6f
fullstack99 <success1989127@gmail.com>
2019-10-08 17:29:12 +0530

    About Us - Tools, SocialTrading, Signals, Featurs

--- src/components/Tools/index.js
+++ src/components/Tools/index.js
@@ -1,84 +1,42 @@
 import React from "react"
 
-const Tools = () => {
+import Container from "react-bootstrap/Container"
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+
+const Tools = ({ data: { preHeading, heading, text, tools_boxes } }) => {
   return (
     <section className="features">
-      <div className="container">
-        <div className="row">
-          <div className="col-12">
+      <Container>
+        <Row>
+          <Col className="col-12">
             <div className="mb-5 pb-5 text-center">
-              <p className="u-color-primary u-weight-smb u-ls-lg">TOOLS</p>
-              <h2 className="mb-3 mt-2 u-color-mirage">Tools Coingenius</h2>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              <p className="u-color-primary u-weight-smb u-ls-lg">
+                {preHeading}
               </p>
+              <h2 className="mb-3 mt-2 u-color-mirage">{heading}</h2>
+              <p>{text}</p>
             </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="col-6 col-md-4">
-            <div className="features__box">
-              <img src="images/icon-placeholder.png" alt="Icon" />
-              <h4 className="mt-4 mb-3">News</h4>
-              <p className="p--md">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </div>
-          <div className="col-6 col-md-4">
-            <div className="features__box">
-              <img src="images/icon-placeholder.png" alt="Icon" />
-              <h4 className="mt-4 mb-3">Signals</h4>
-              <p className="p--md">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </div>
-          <div className="col-6 col-md-4">
-            <div className="features__box">
-              <img src="images/icon-placeholder.png" alt="Icon" />
-              <h4 className="mt-4 mb-3">Overs</h4>
-              <p className="p--md">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </div>
-          <div className="col-6 col-md-4">
-            <div className="features__box">
-              <img src="images/icon-placeholder.png" alt="Icon" />
-              <h4 className="mt-4 mb-3">Prediction</h4>
-              <p className="p--md">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </div>
-          <div className="col-6 col-md-4">
-            <div className="features__box">
-              <img src="images/icon-placeholder.png" alt="Icon" />
-              <h4 className="mt-4 mb-3">Consumer Sentament</h4>
-              <p className="p--md">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </div>
-          <div className="col-6 col-md-4">
-            <div className="features__box">
-              <img src="images/icon-placeholder.png" alt="Icon" />
-              <h4 className="mt-4 mb-3">Alerts or Push</h4>
-              <p className="p--md">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
+          </Col>
+        </Row>
+        <Row>
+          {tools_boxes.map((item, index) => (
+            <Col xs={6} md={4} key={index}>
+              <div className="features__box">
+                <img
+                  className="mb-0"
+                  src="http://nickshanekearney.com/webPreview/images/icon-placeholder.png"
+                  alt="Icon"
+                />
+                <h4 className="mt-4 mb-3 text-midnight-express">
+                  {item.tool_title}
+                </h4>
+                <p className="p--md">{item.tool_text}</p>
+              </div>
+            </Col>
+          ))}
+        </Row>
+      </Container>
     </section>
   )
 }
--- src/pages/about.js
+++ src/pages/about.js
@@ -5,18 +5,38 @@ import Layout from "@components/Layout"
 import SEO from "@components/Seo"
 import Hero from "@components/Hero"
 import Tools from "@components/Tools"
+import Signals from "@components/Signals"
+import Features from "@components/Features"
+import SocialTrading from "@components/SocialTrading"
 
 const MediaPage = ({ data }) => {
-  const { hero } = data.allAboutJson.edges[0].node
+  const {
+    hero,
+    tools,
+    signals,
+    features,
+    socialTrading
+  } = data.allAboutJson.edges[0].node
 
-  const { HeroImage } = data
+  const { HeroImage, marketDataImage, SocialTradingImage, calendarImage } = data
 
   return (
     <Layout>
       <SEO title="Media" />
       <div className="about">
         <Hero data={hero} fluid={HeroImage.childImageSharp.fluid} />
-        <Tools />
+        <Tools data={tools} />
+        <Signals data={signals} />
+        <Features
+          data={features}
+          fluid={marketDataImage.childImageSharp.fluid}
+          calender={calendarImage.childImageSharp.fluid}
+        />
+        <SocialTrading
+          data={socialTrading}
+          fluid={SocialTradingImage.childImageSharp.fluid}
+          calender={calendarImage.childImageSharp.fluid}
+        />
       </div>
     </Layout>
   )
@@ -33,10 +53,65 @@ export const query = graphql`
             text
             cta
           }
+          tools {
+            preHeading
+            heading
+            text
+            tools_boxes {
+              tool_title
+              tool_text
+            }
+          }
+          signals {
+            preHeading
+            heading
+            text
+            signals_boxes {
+              signal_title
+              signal_text
+            }
+          }
+          features {
+            preHeading
+            heading
+            features_boxes {
+              feature_title
+              feature_text
+            }
+          }
+          socialTrading {
+            preHeading
+            heading
+            features_boxes {
+              feature_title
+              feature_text
+            }
+          }
         }
       }
     }
     HeroImage: file(relativePath: { eq: "illustration.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 500, quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    marketDataImage: file(relativePath: { eq: "float.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 500, quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    SocialTradingImage: file(relativePath: { eq: "UI-illustration.png" }) {
+      childImageSharp {
+        fluid(maxWidth: 500, quality: 100) {
+          ...GatsbyImageSharpFluid_withWebp
+        }
+      }
+    }
+    calendarImage: file(relativePath: { eq: "i-calendar.png" }) {
       childImageSharp {
         fluid(maxWidth: 500, quality: 100) {
           ...GatsbyImageSharpFluid_withWebp
