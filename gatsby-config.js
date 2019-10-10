01bde87bea3875140bf4e07c4abb9758b8749469
fullstack99 <success1989127@gmail.com>
2019-10-09 23:48:06 -0600

    Google Tag Manager

--- gatsby-config.js
+++ gatsby-config.js
@@ -126,6 +126,8 @@ module.exports = {
         // should be an object or a function that is executed in the browser
         // Defaults to null
         defaultDataLayer: { platform: "gatsby" },
+
+        gtmAuth: "4OjORx1z4RMUqWxbxB6yzEIJvZUOQSbmjFf0lPwe8iU",
       },
     },
     {
