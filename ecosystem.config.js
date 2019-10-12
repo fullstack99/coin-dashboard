27f34b3dceb37d6e57213138d6226b44c60e2e36
fullstack99 <success1989127@gmail.com>
2019-10-11 22:01:20 -0600

    Fix Production deployment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,39 +1,44 @@
 const productionConfig = {
-  user: 'bitnami',
-  host: '34.213.212.52',
-  ref: 'origin/master',
-  repo: 'git@bitbucket.org:gocodistry/dashboard.git',
-  path: '/opt/bitnami/apps/coingenius.ai',
+  user: "bitnami",
+  host: "34.213.212.52",
+  ref: "origin/master",
+  repo: "git@bitbucket.org:gocodistry/dashboard.git",
+  path: "/opt/bitnami/apps/coingenius.ai",
   // 'post-deploy':
   //   'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --name AuthWebsite',
   env: {
-    NODE_ENV: 'production'
+    NODE_ENV: "production"
   }
 }
 module.exports = {
-  apps : [{
-    name: 'Dashboard',
-    script: 'npm run serve',
+  apps: [
+    {
+      name: "Dashboard",
+      script: "npm run serve",
 
-    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-    // args: 'one two',
-    instances: 1,
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
+      // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+      // args: 'one two',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "development"
+      },
+      env_production: {
+        NODE_ENV: "production"
+      }
     }
-  }],
+  ],
 
-  deploy : {
-    production : {
+  deploy: {
+    production: {
       ...productionConfig,
-      key: '~/gocodistry/Coingenious.pem'
+      key: "~/gocodistry/Coingenious.pem"
     },
-    productionpipeline : productionConfig
+    productionpipeline: {
+      ...productionConfig,
+      key: "~/.ssh/id_rsa"
+    }
   }
-};
+}
