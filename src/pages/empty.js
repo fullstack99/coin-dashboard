a71d7781bd8c30a7af45fc712124840670e1f75b
fullstack99 <success1989127@gmail.com>
2019-10-11 21:17:50 -0600

    Remove debug code for test page and console.log for empty page

--- src/pages/empty.js
+++ src/pages/empty.js
@@ -3,7 +3,6 @@ import { navigate } from "gatsby"
 
 const Empty = ({ location }) => {
   const { pathname } = location
-  console.log({ location })
   useEffect(() => {
     if (pathname.indexOf("/empty") === 0) {
       navigate("/")
--- src/pages/test.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const TestPage = () => (<div>Test page</div>)
-
-export default TestPage
